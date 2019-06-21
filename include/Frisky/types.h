bf28e446229de1bbb24c267b31d33338ad17a074
Tufted Tail <tufted.tail@gmail.com>
2019-06-21 11:47:29 -0700

    more types

--- include/Frisky/types.h
+++ include/Frisky/types.h
@@ -4,10 +4,20 @@
 #include <cstddef>
 #include <cstdint>
 
+struct SDL_Window;
+struct SDL_Renderer;
+struct SDL_Texture;
+struct SDL_Rect;
+
 namespace frisky {
   using size_t     = std::size_t;
   using uintptr_t  = std::uintptr_t;
   using sdlflags_t = std::uint32_t;
+
+  using window_rptr_t   = SDL_Window*;
+  using renderer_rptr_t = SDL_Renderer*;
+  using texture_rptr_t  = SDL_Texture*;
+  using rect_rptr_t     = SDL_Rect*;
 }
 
 #endif  // FRISKY_TYPES_H_
